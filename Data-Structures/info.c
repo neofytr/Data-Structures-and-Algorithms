1e7ae5e12cf720831c9ca513458dcc93369cb7a2
RajDaBest <imagination12344321.com@gmail.com>
2024-12-14 07:35:47 +0530

    first commit

---
+++ Data-Structures/info.c
@@ -0,0 +1,11 @@
+/*
+
+A data structure is a method to realize a set of operations on some data.
+
+In general, a data structure is a kind of higher-level instruction in a virtual machine: when an
+algorithm needs to execute some operations many times, it is reasonable to identify
+what exactly the needed operations are and how they can be realized in the most efficient way.
+This is the basic question of data structures: given a set of operations whose intended behavior is known,
+how should we realize that behavior?
+
+*/
